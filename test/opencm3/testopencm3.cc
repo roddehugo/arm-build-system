251ba736b5e239f11db7c0f45873a20820d498d5
Hugo Rodde <rodde.hugo@gmail.com>
2020-04-10 00:22:10 +0200

    No need to number led on bluepill and add missing return

--- test/opencm3/testopencm3.cc
+++ test/opencm3/testopencm3.cc
@@ -1,21 +1,25 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
-#define RCC_LED1 RCC_GPIOC
-#define PORT_LED1 GPIOC
-#define PIN_LED1 GPIO13
+#define LEDRCC (RCC_GPIOC)
+#define LEDPORT (GPIOC)
+#define LEDPIN (GPIO13)
+#define GPIO_MODE (GPIO_MODE_OUTPUT_2_MHZ)
+#define GPIO_PUPD (GPIO_CNF_OUTPUT_PUSHPULL)
 
 int
 main ()
 {
-    rcc_periph_clock_enable (RCC_LED1);
-    gpio_set_mode (PORT_LED1, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, PIN_LED1);
-    gpio_set (PORT_LED1, PIN_LED1);
+    rcc_periph_clock_enable (LEDRCC);
+    gpio_set_mode (LEDPORT, GPIO_MODE, GPIO_PUPD, LEDPIN);
+    gpio_set (LEDPORT, LEDPIN);
 
     while (1)
     {
         for (int i = 0; i < 200000; ++i)
             asm volatile ("nop");
-        gpio_toggle (PORT_LED1, PIN_LED1);
+        gpio_toggle (LEDPORT, LEDPIN);
     }
+
+    return 0;
 }
