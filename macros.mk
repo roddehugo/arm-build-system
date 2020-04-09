4ae98789d4ebb33b2949c1630c52541d9c4ac146
Hugo Rodde <rodde.hugo@gmail.com>
2020-04-09 11:26:40 +0200

    Give back credit to ucoolib for the macros

--- macros.mk
+++ macros.mk
@@ -1,3 +1,5 @@
+# ucoolib - Microcontroller object oriented library.
+
 # Run mkdir if directory does not exists.
 # $(call mkdir_if_needed,FILE)
 mkdir_if_needed = $(if $(wildcard $(dir $1)),,$(Q)$(MKDIR) $(dir $1))
