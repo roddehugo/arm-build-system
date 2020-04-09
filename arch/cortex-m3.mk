3d239d97fd5a27b01886fb1310d4adcd7dd02eb5
Hugo Rodde <rodde.hugo@gmail.com>
2020-04-09 23:46:13 +0200

    Fix misuse of make's origin function, should pass the variable name
    This fixes bab19723.

--- arch/cortex-m3.mk
+++ arch/cortex-m3.mk
@@ -6,4 +6,4 @@ ARCH_FLAGS += -mcpu=cortex-m3 # Use Cortex-M3 processor.
 ARCH_FLAGS += -mfloat-abi=soft # Use soft floating-point ABI.
 
 # Cortex-M3 arch definitions.
-ARCH_DEFS += $(call ifdefined,$(FAMILY),-D$(call toupper,$(FAMILY)))
+ARCH_DEFS += $(call ifdefined,FAMILY,-D$(call toupper,$(FAMILY)))
